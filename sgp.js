f2da22e9dd6c0d4d9187194302ae3a9e6cfa6295
Julio_cesar <julio.oliveira04@escola.pr.gov.br>
2026-04-29 13:52:27 +0000

    versão2

--- sgp.js
+++ sgp.js
@@ -14,8 +14,20 @@ function filtrarMissoesConcluidas(listaDeMissoes) {
     const verificarStatusDamissao = function(missao) {
         const foiFinalizada = missao.status === "concluida"; //=== igualdade ESTRITA
         return foiFinalizada;
-    }
+    };
 
     const apenasConcluidas = listaDeMissoes.filter(verificarStatusDamissao);
     return apenasConcluidas;
+};
+
+function atualizarInventario(inventarioAtual, acao, nomeDoItem) {
+    let novoInventario;
+
+    if (acao === "pegar") {
+        const inventarioComItemNovo = [inventarioAtual, nomeDoItem]
+        novoInventario = inventarioComItemNovo;
+    } else if (acao === "descartar") //Parei aqui
 }
+
+const pontosDoJogador = [100, 50, 200, 10]
+console.log(pontosDoJogador);
