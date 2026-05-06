972b76dcd359c8c4cb8d54c6900e2c8aa589e1ab
Julio_cesar <julio.oliveira04@escola.pr.gov.br>
2026-05-06 13:49:38 +0000

    versão4

--- sgp.js
+++ sgp.js
@@ -24,10 +24,30 @@ function atualizarInventario(inventarioAtual, acao, nomeDoItem) {
     let novoInventario;
 
     if (acao === "pegar") {
-        const inventarioComItemNovo = [inventarioAtual, nomeDoItem]
+        const inventarioComItemNovo = [inventarioAtual, nomeDoItem];
         novoInventario = inventarioComItemNovo;
-    } else if (acao === "descartar") //Parei aqui
-}
+    } else if (acao === "descartar") {
+        const inventarioSemUltimoItem = inventarioAtual.slice();
+        novoInventario = inventarioSemUltimoItem;
+    } else {
+        novoInventario = inventarioAtual;
+    }
+    return novoInventario; //retornar a mochila do jogador atualizada
+};
 
 const pontosDoJogador = [100, 50, 200, 10]
-console.log(pontosDoJogador);
+const total = calcularPontuacaoTotal
+console.log("pontuação final: " , total);
+
+const missoes = [
+    {nome: "Salvar a aldeia", status: "concluida"},
+    {nome: "Encontrar o mapa", status: "em andamento"},
+    {nome: "Derrotar o chefão", status: "falhou"},
+    {nome: "Coletar recursos", status: "concluida"}
+];
+
+const concluidas = filtrarMissoesConcluidas(missoes)
+console.log("Missoes concluidas: " , concluidas);
+
+const mochila = ["Mapa", "Lanterna"];
+const mochilaAtualizada = atualizarInventario(mochila, "pegar", "Bússola");
